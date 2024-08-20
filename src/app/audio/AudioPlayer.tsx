31a49c1818d62c47fcc8b8ae79fc8a4f75bf2de6
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-20 23:53:57 +0600

    audio player update #2

--- src/app/audio/AudioPlayer.tsx
+++ src/app/audio/AudioPlayer.tsx
@@ -12,6 +12,17 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({ onAudioContextReady }) => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const audioContextRef = useRef<AudioContext | null>(null);
 
+  useEffect(() => {
+    const handleInteraction = () => {
+      if (!audioContextRef.current) {
+        const audioContext = new (window.AudioContext ||
+          (window as any).webkitAudioContext)();
+        audioContextRef.current = audioContext;
+        onAudioContextReady(audioContext, audioRef.current as HTMLAudioElement);
+      } else if (audioContextRef.current.state === "suspended") {
+        audioContextRef.current.resume();
+      }
+    };
 
     // Listen for a user interaction
     document.addEventListener("click", handleInteraction);
