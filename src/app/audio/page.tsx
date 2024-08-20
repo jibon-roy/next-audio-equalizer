8d14041f0b2de642adce563d4e13231a9fa2e1d9
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-20 23:51:57 +0600

    audio player main page update

--- src/app/audio/page.tsx
+++ src/app/audio/page.tsx
@@ -1,21 +1,25 @@
 "use client";
-import React, { useState } from 'react';
-import AudioPlayer from './AudioPlayer';
-import Equalizer from '../components/Equlizer';
-
+import React, { useState } from "react";
+import AudioPlayer from "./AudioPlayer";
+import Equalizer from "../components/Equlizer";
 
 const AudioEqualizer: React.FC = () => {
   const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
-  const [audioElement, setAudioElement] = useState<HTMLAudioElement | null>(null);
+  const [audioElement, setAudioElement] = useState<HTMLAudioElement | null>(
+    null
+  );
 
-  const handleAudioContextReady = (audioContext: AudioContext, audioElement: HTMLAudioElement) => {
+  const handleAudioContextReady = (
+    audioContext: AudioContext,
+    audioElement: HTMLAudioElement
+  ) => {
     setAudioContext(audioContext);
     setAudioElement(audioElement);
   };
 
   return (
     <div>
-      <AudioPlayer onAudioContextReady={handleAudioContextReady} />
+      {/* <AudioPlayer onAudioContextReady={handleAudioContextReady} /> */}
       <Equalizer audioContext={audioContext} audioElement={audioElement} />
     </div>
   );
